0ada41cf3943d9146ce6bf2a735891a423c0f77f
stefanius <sgrootveld@gmail.com>
2013-06-18 23:52:41 +0200

    Gehaktdag toegevoegd

--- app/Controller/CheckdayController.php
+++ app/Controller/CheckdayController.php
@@ -7,7 +7,6 @@ class CheckdayController extends AppController {
     function zaagmans(){
         $zaagmansGeweest = false;
         $daynumber =  (int)date ("N");
-        $hour =  (int)date ("H");
         
         if($daynumber === 3){
             $hour =  (int)date ("H");
@@ -22,6 +21,19 @@ class CheckdayController extends AppController {
         $description = 'Bekijk hier dagelijks of Zaagmans al is geweest. Zaagmaans komt elke woensdag langs op Dag Van De Week. Elke week weer een zaagmans.';
         $robots = 'INDEX, FOLLOW';
         $this->set(compact('zaagmansGeweest', 'description', 'robots'));
-    }    
+    }   
+    
+    function gehaktdag(){
+        $gehaktdag = false;
+        $daynumber =  (int)date ("N");
+        
+        if($daynumber === 3){
+            $gehaktdag=true;
+        }
+        
+        $description = 'Woensdag Gehaktdag. Bekijk hier dagelijks of het al gehaktdag is. Elke woensdag is Gehaktdag op Dag Van De Week. Elke week weer een gehaktdag.';
+        $robots = 'INDEX, FOLLOW';
+        $this->set(compact('zaagmansGeweest', 'description', 'robots'));
+    }
 }
 
---
+++ app/View/Checkday/gehaktdag.ctp
@@ -0,0 +1,7 @@
+<?php $this->html->meta('keywords','enter any meta keyword here'); ?>
+
+<?php if($gehaktdag===true): ?>
+    Vandaag is <strong>Woensdag Gehaktdag</strong>. Geniet ervan!
+<?php else: ?>
+    Helaas. Het is nog geen gehaktdag. U moet nog X dagen wachten.
+<?php endif; ?>
